1e9c40f4635fc63aa95417ae84e9143a69d47135
printf.soyam <socialsoyam@gmail.com>
2025-09-29 20:00:57 +0530

    Adding SquarePattern

---
+++ SquarePattern.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class SquarePattern{
+    public static void main(String[] args){
+
+        for(int Line = 1; Line <= 5; Line++){
+            System.out.println("* * * *");
+        }
+    }
+}
